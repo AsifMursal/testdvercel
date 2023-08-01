544d7a6719709f0abbeb9c0c3e73a6c3df01cb20
Asif Mursal <asif.mursal21@vit.edu>
2023-08-01 14:45:35 +0530

    staticroot

--- testpro/settings.py
+++ testpro/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# MEDIA_URLS ='/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
